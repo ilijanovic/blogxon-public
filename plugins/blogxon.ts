f2203f928745ae14b42ca22f8643ac993190433c
ilijanovic <ilija.marijanovic@gmx.at>
2020-11-27 14:53:27 +0100

    update again

--- plugins/blogxon.ts
+++ plugins/blogxon.ts
@@ -30,7 +30,7 @@ export default function (context: Context, inject: Inject) {
       }
       const subscribeOptions = {
         userVisibleOnly: true,
-        applicationServerKey: Uint8Array.from(atob(PUBLIC_KEY), c => c.charCodeAt(0))
+        applicationServerKey: urlBase64ToUint8Array(PUBLIC_KEY),
       }
       let subscription = await registration.pushManager.subscribe(
         subscribeOptions
@@ -49,4 +49,16 @@ export default function (context: Context, inject: Inject) {
   })
 }
 
+function urlBase64ToUint8Array(base64String: string) {
+  var padding = '='.repeat((4 - (base64String.length % 4)) % 4)
+  var base64 = (base64String + padding)
+    .replace(/\-/g, '+')
+    .replace(/_/g, '/')
 
+  var rawData = window.atob(base64)
+  var outputArray = new Uint8Array(rawData.length)
+  for (var i = 0; i < rawData.length; ++i) {
+    outputArray[i] = rawData.charCodeAt(i)
+  }
+  return outputArray
+}
