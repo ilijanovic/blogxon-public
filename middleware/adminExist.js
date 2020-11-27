96c5a76a2ac2a214adf9045f4c9b4571b921a799
ilijanovic <ilija.marijanovic@gmx.at>
2020-11-27 13:50:26 +0100

    update middleware

--- middleware/adminExist.js
+++ middleware/adminExist.js
@@ -1,7 +1,9 @@
+import { config } from '~/config'
+
 export default async function ({ $axios, redirect }) {
   let host =
     process.env.NODE_ENV === 'production'
-      ? process.env.HOST
+      ? config.domain
       : `http://localhost:3000`
   let adminExist = await $axios.$get(host + '/api/internal/adminExist')
   if (adminExist) return redirect('/')
